f218822b2e7e8be774f22b6dfe4bb879f64a0230
Magersers <kalabkin2015@mail.ru>
2025-02-13 11:49:27 +0300

    initial commit

--- mat_app/views.py
+++ mat_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import numpy as np
 
+def index(site):
+    arr1 = np.array([1,2,3,4])
+    mal = {'name':arr1}
+    return render(site,'index.html',context=mal)
 # Create your views here.
